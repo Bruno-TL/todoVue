efe760eb78a5b677d28ce06030c0452b33a29ea8
Thiago <tbgt35@hotmail.com>
2023-04-05 10:07:05 -0300

    aj

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar>
+    <!-- <v-navigation-drawer v-model="drawer" app>
+      
+    </v-navigation-drawer> -->
+    <v-navigation-drawer v-model="drawer" app>
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              Application
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-list dense class="pt-0">
+        <v-list-tile v-for="item in items" :key="item.title" @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
-    
-    <v-navigation-drawer v-model="drawer">
-      <!--  -->
-    </v-navigation-drawer>
-
-    
 
     <v-main>
       <!--  -->
@@ -19,7 +42,13 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({ drawer: null }),
-  }
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+      { title: 'About', icon: 'mdi-help-box' },
+    ]
+  }),
+}
 </script>
